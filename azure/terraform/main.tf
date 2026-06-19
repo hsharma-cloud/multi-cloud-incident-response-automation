67ae84ce244b1dcccd379f64cbdbd983c9f5a0ff
Hari Sharma <your-github-email@example.com>
2026-06-18 21:28:51 -0400

    Add Azure monitoring and security infrastructure

--- azure/terraform/main.tf
+++ azure/terraform/main.tf
@@ -23,3 +23,29 @@ resource "azurerm_network_security_group" "workload" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "law-multicloud-ir-dev"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "main" {
+  name                = "appi-multicloud-ir-dev"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  workspace_id        = azurerm_log_analytics_workspace.main.id
+  application_type    = "web"
+}
+
+
+resource "azurerm_storage_account" "main" {
+  name                     = "stmulticloudirdev01"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
